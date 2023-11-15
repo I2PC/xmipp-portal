0061259af7cf37e63908724f72f98c65be6c7afd
albertmena <12760268+albertmena@users.noreply.github.com>
2023-11-15 11:53:52 +0100

    tremendo push

--- web/serializers.py
+++ web/serializers.py
@@ -1,6 +1,36 @@
-from .models import Attempt
+from .models import Attempt, User, Version, Xmipp
 from rest_framework.serializers import ModelSerializer
+
+class UserSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['ID', 'country']
+class XmippSerializer(ModelSerializer):
+    class Meta:
+        model = Xmipp
+        fields = ['branch', 'updated']
+
+class VersionsSerializer(ModelSerializer):
+    class Meta:
+        model = Version
+        fields = ['os', 'cudaVersion', 'cmakeVersion', 'gccVersion', 'gppVersion', 'sconsVersion']
+
 class AttemptSerializer(ModelSerializer):
+    user = UserSerializer()
+    xmipp = XmippSerializer()
+    version = VersionsSerializer()
     class Meta:
         model = Attempt
         fields = ['user', 'version', 'xmipp', 'date', 'returnCode', 'logTail']
+
+    def create(self, validated_data):
+            user_data = validated_data.pop('user')
+            version_data = validated_data.pop('version')
+            xmipp_data = validated_data.pop('xmipp')
+
+            user, _ = User.objects.get_or_create(**user_data)
+            version, _ = Version.objects.get_or_create(**version_data)
+            xmipp, _ = Xmipp.objects.get_or_create(**xmipp_data)
+
+            attempt = Attempt.objects.create(user=user, version=version, xmipp=xmipp, **validated_data)
+            return attempt
--- web/views.py
+++ web/views.py
@@ -17,43 +17,61 @@ class AttemptsView(APIView):
 
     def post(self, request, format='json'):
         serializer = self.serializer_class(data=request.data)
-        if serializer.is_valid() and format == 'json':
-            data = serializer.data
+        print('serializer.is_valid(): {}'.format(serializer.is_valid()))
+        print('''format == 'json': {}'''.format(format == 'json'))
 
-            userID = data.id
-            date = data.date
-            os = data.os
-            cuda = data.cuda
-            cmake = data.cmake
-            gcc = data.gcc
-            gpp = data.gpp
-            scons = data.scons
-            branch = data.branch
-            updated = data.updated
-            returnCode = data.returnCode
-            logTail = data.logTail
+        if serializer.is_valid() and format == 'json':
+            print('serializer.errors: {}'.format(serializer.errors))
+
+            validated_data = serializer.validated_data
+            user_data = validated_data.get('user')
+            version_data = validated_data.get('version')
+            xmipp_data = validated_data.get('xmipp')
+            returnCode = validated_data.get('returnCode')
+            logTail = validated_data.get('logTail')
+
+            print('Espabila')
+
+
+            #userID = serializer.user.ID
+            #date = data.date TODO manage date from django
+            #country = data.user.country TODO manage country from django
+            #os = serializer.version.os
+            #cuda = serializer.version.cudaVersion
+            #cmake = serializer.version.cmakeVersion
+            #gcc = serializer.version.gccVersion
+            #gpp = serializer.version.gppVersion
+            #scons = serializer.version.sconsVersion
+            #branch = serializer.xmipp.branch
+            #updated = serializer.xmipp.updated
+            #returnCode = serializer.attempt.returnCode
+            #logTail = serializer.attempt.logTail
 
             userObj, created = User.objects.get_or_create(
-                ID=userID,
-                country=getCountryFromIP(getClientIP(request)))
+                ID=user_data['ID'],
+                country='CoreaDelNorte')
+
+            print('Hello2')
 
             xmippObj, created = Xmipp.objects.get_or_create(
-                branch=branch,
-                updated=updated)
+                branch=xmipp_data['branch'],
+                updated=xmipp_data['updated'])
+            print('Hello3')
 
             versionsObj, created = Version.objects.get_or_create(
-                os=os,
-                cuda=cuda,
-                cmake=cmake,
-                gpp=gpp,
-                gcc=gcc,
-                scons=scons)
+                os=version_data['os'],
+                cudaVersion=version_data['cudaVersion'],
+                cmakeVersion=version_data['cmakeVersion'],
+                gppVersion=version_data['gppVersion'],
+                gccVersion=version_data['gccVersion'],
+                sconsVersion=version_data['sconsVersion'])
+            print('Hello4')
 
 
             attempt = Attempt(user=userObj,
                               version=versionsObj,
-                              smipp=xmippObj,
-                              date=date,
+                              xmipp=xmippObj,
+                              #date=date,
                               returnCode=returnCode,
                               logTail=logTail)
             attempt.save()
@@ -63,6 +81,7 @@ class AttemptsView(APIView):
             #return JsonResponse(AttemptSerializer(attempt).data, status=status.HTTP_201_CREATED)
 
         else:
+            print('serializer.errors: {}'.format(serializer.errors))
             return Response({'Holi ': 0}, status=status.HTTP_400_BAD_REQUEST)
 
 
@@ -98,6 +117,34 @@ request json example:
       'returnCode': returnCode,
       'logTail': logTail
     }
+curl --header "Content-Type: application/json" -X POST --data '{"user":"hashMachine","os":"Ubuntur","cuda":"NoSequeeseso","cmake":"3.5.6","gcc":"4.perocentos","gpp":"gepusplas","scons":"4.3.3","branch":"agm_API","updated":"claroMakina","returnCode":"0 con espacio ", "logTail":"muchas lines"}' --request POST http://127.0.0.1:8000/web/attempts/
+
+curl --header "Content-Type: application/json" \
+     -X POST \
+     --data '{
+       "user": {
+         "ID": "hashMachine",
+         "country": "someCountry"
+       },
+       "version": {
+         "os": "Ubuntur",
+         "cudaVersion": "NoSequeeseso",
+         "cmakeVersion": "3.5.6",
+         "gccVersion": "4.perocentos",
+         "gppVersion": "gepusplas",
+         "sconsVersion": "4.3.3"
+       },
+       "xmipp": {
+         "branch": "agm_API",
+         "updated": true
+       },
+       "returnCode": "0 con espacio",
+       "logTail": "muchas lines"
+     }' \
+     http://127.0.0.1:8000/web/attempts/
+
+
+
 
 '''
 
