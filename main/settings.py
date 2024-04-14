512306002e2c712898960bb34d931af253731caf
Carolina Simón <carolina.simon.guerrero@gmail.com>
2024-04-14 19:25:59 +0200

    Add secret key to .env

--- main/settings.py
+++ main/settings.py
@@ -21,7 +21,7 @@ load_dotenv(os.path.join(BASE_DIR, ".env"))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'in##hj=!8!%v+%i0f!9a(#eq^bgc6lnz^lkt%rkgql*0=2tm&^'
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ["IS_DEV"]
