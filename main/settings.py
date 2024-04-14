2b81d8e1d2d16635bc3df284c11406d842b48223
Carolina Simón <carolina.simon.guerrero@gmail.com>
2024-04-14 17:04:37 +0200

    Add local ip as allowed hosts

--- main/settings.py
+++ main/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'in##hj=!8!%v+%i0f!9a(#eq^bgc6lnz^lkt%rkgql*0=2tm&^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ["IS_DEV"]
 
-ALLOWED_HOSTS = ['xmipp.i2pc.es']
+ALLOWED_HOSTS = ['xmipp.i2pc.es', '127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
