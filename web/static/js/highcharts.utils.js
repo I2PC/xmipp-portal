47255e27e4098da6ea34072f736971a84314d368
albertmena <12760268+albertmena@users.noreply.github.com>
2024-11-18 12:49:28 +0100

    fixing datetime sorting

--- web/static/js/highcharts.utils.js
+++ web/static/js/highcharts.utils.js
@@ -74,70 +74,51 @@ function loadBarChart(container, title, data){
 }
 
 function prepareSeriesForTimeChart(data, name) {
+    console.log('data', data)
     const colorPalette = ['#c12e2a', '#8e1919', '#540000', '#d9534f', '#DBD9D9', '#808080'];
     let seriesData = {};
-
-    // Función para obtener el timestamp del lunes de la semana
     function getStartOfWeek(date) {
         const d = new Date(date);
         const day = d.getDay();
-        const diff = d.getDate() - day + (day == 0 ? -6 : 1); // Si es domingo (0), retrocedemos 6 días, sino retrocedemos 1
+        const diff = d.getDate() - day + (day == 0 ? -6 : 1);
         d.setDate(diff);
-        d.setHours(0, 0, 0, 0); // Establecer la hora a las 00:00:00 para el lunes
-        return d; // Devolvemos el objeto Date del lunes (no el timestamp por ahora)
+        d.setHours(0, 0, 0, 0);
+        return d
     }
 
-    // Procesamos los datos de entrada
+
     for (let item of data) {
-        const branch = item.xmipp__branch; // Obtenemos la rama (branch)
+        const branch = item.xmipp__branch;
         if (!seriesData[branch]) {
-            // Si no existe la rama en seriesData, la creamos
             seriesData[branch] = {
                 name: branch,
-                dataSorting: { enabled: true },
-                data: [], // Inicializamos un array vacío para almacenar los datos de la rama
+                dataSorting: { enabled: false },
+                data: [],
             };
         }
-
-        const weekStart = getStartOfWeek(item.date); // Obtenemos el lunes de la semana de la fecha
-        const weekStamp = weekStart.getTime(); // Convertimos el lunes en un timestamp
-
+        const weekStart = getStartOfWeek(item.date);
+        const weekStamp = weekStart.getTime();
         let found = false;
-        for (let entry of seriesData[branch].data) {
-            // Verificamos si ya existe un dato con el mismo timestamp de la semana
-            if (entry[0] === weekStamp) {
-                entry[1]++; // Si existe, aumentamos el contador
+        for (let [index, entry] of seriesData[branch].data.entries()) {
+            if (entry.x === weekStamp) {
+                seriesData[branch].data[index].y = seriesData[branch].data[index].y + 1
                 found = true;
                 break;
             }
         }
-
         if (!found) {
-            // Si no encontramos el dato, lo agregamos con un contador inicial de 1
-            seriesData[branch].data.push([weekStamp, 1]);
+            seriesData[branch].data.push({x : weekStamp, y: 1});
         }
     }
-
-    // Creamos el array final para la serie
     let colorIndex = 0;
     let series = [];
     for (let branch in seriesData) {
-        let branchData = seriesData[branch];
-        branchData.color = colorPalette[colorIndex]; // Asignamos un color a la rama
-        let dataArray = [];
-
-        // Copiamos los datos de la rama en un nuevo array
-        for (let entry of branchData.data) {
-            console.log(entry); // Verificación: mostramos los datos que se agregan
-            dataArray.push([entry[0], entry[1]]); // [timestampSemana, contador]
+        seriesData[branch].color = colorPalette[colorIndex];
+        colorIndex = (colorIndex + 1) % colorPalette.length;
+        series.push(seriesData[branch]);
         }
-
-        branchData.data = dataArray; // Asignamos los datos formateados a la rama
-        colorIndex = (colorIndex + 1) % colorPalette.length; // Rotamos los colores
-        series.push(branchData); // Agregamos la rama a la serie final
-    }
-
-    return series; // Retornamos el array de series
+       console.log(series)
+    return series;
 }
 
 
@@ -156,8 +137,8 @@ function loadTimeChart(container, title, data){
         },
         xAxis: {
             type: "datetime",
-            minRange: 7 * 24 * 3600 * 1000, // Intervalo mínimo de una semana
-            tickInterval: 7 * 24 * 3600 * 1000,
+//            minRange:  3600 * 1000, // Intervalo mínimo de una semana
+//            tickInterval:  3600 * 1000,
             labels: {
                 style: {
                     fontSize: '12px', 
@@ -172,7 +153,7 @@ function loadTimeChart(container, title, data){
                     fontFamily: 'Verdana, sans-serif'
                 }
             },
-            minTickInterval: 1,  // Esto asegura que el intervalo mínimo entre marcas de ticks es 1
+            //minTickInterval: 1,  // Esto asegura que el intervalo mínimo entre marcas de ticks es 1
             allowDecimals: false,  // Esto evitará que los valores del eje Y tengan decimales
 
         },
@@ -187,7 +168,7 @@ function loadTimeChart(container, title, data){
                     rotation: 0,
                     color: '#000000',
                     align: 'left',
-                    format: '{point.y}', // one decimal
+                    //format: '{point.y}', // one decimal
                     y: -10, // 10 pixels down from the top
                     x: 0, // 0 pixels
                     style: {
