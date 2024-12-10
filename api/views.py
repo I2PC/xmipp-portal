514481ba0fa926226ae0cc2cf51f81c8cb2a8c59
csimong <carolina.simon.guerrero@gmail.com>
2024-12-10 11:10:18 +0100

    update curl example in views.py

--- api/views.py
+++ api/views.py
@@ -404,26 +404,32 @@ class AttemptsView(APIView):
       )
 
 '''
-curl --header "Content-Type: application/json" -X POST --data '{
-        "user": {
-        "userId": "hashMachine5", "country": ""
-        },
-        "version": {
-        "os": "Centor",
-        "architecture": "skylaque",
-        "cuda": "NoSequeeseso",
-        "cmake": "3.5.6",
-        "gcc": "4.perocentos",
-        "gpp": "gepusplas",
-        },
-        "xmipp": {
-        "branch": "agm_API",
-        "updated": true,
-        "installedByScipion": true,
-        },
-        "returnCode": "0",
-        "logTail": "muchas lines"
-        }' --request POST http://127.0.0.1:8000/api/attempts/ > file.html
+ curl --header "Content-Type: application/json" -X POST --data '{
+"user": {
+"userId": "machineHash"
+}, 
+"version": {
+"os": "Ubuntu 22.04.5 LTS", 
+"architecture": "fpu vme de pse tsc msr pae mce cx8 apic sep mtrr pge mca cmov pat pse36 clflush dts acpi mmx fxsr sse sse2 ss ht tm pbe syscall nx pdpe1gb rdtscp lm constant_tsc art arch_perfmon pebs bts rep_good nopl xtopology nonstop_tsc cpuid aperfmperf pni pclmulqdq dtes64 monitor ds_cpl vmx est tm2 ssse3 sdbg fma cx16 xtpr pdcm pcid sse4_1 sse4_2 x2apic movbe popcnt tsc_deadline_timer aes xsave avx f16c rdrand lahf_lm abm 3dnowprefetch cpuid_fault epb ssbd ibrs ibpb stibp ibrs_enhanced tpr_shadow flexpriority ept vpid ept_ad fsgsbase tsc_adjust bmi1 avx2 smep bmi2 erms invpcid mpx rdseed adx smap clflushopt intel_pt xsaveopt xsavec xgetbv1 xsaves dtherm ida arat pln pts hwp hwp_notify hwp_act_window hwp_epp vnmi pku ospke md_clear flush_l1d arch_capabilities", 
+"cuda": "11.7.64", 
+"cmake": "3.22.1", 
+"gcc": "GNU-11.4.0", 
+"gpp": "GNU-11.4.0", 
+"mpi": "3.1", 
+"python": "3.8.15", 
+"sqlite": "3.46.0", 
+"java": "11.0.25", 
+"hdf5": "1.10.6", 
+"jpeg": "80"
+}, 
+"xmipp": {
+"branch": "agm_ScipionField", 
+"updated": true, 
+"installedByScipion": true
+}, 
+"returnCode": 0, 
+"logTail": null
+}' --request POST http://127.0.0.1:8000/api/attempts/ > file.html
 
 
 '''
