e55f17d1346d6e4fe923539f6162f4be72cc593a
albertmena <12760268+albertmena@users.noreply.github.com>
2024-12-18 13:29:46 +0100

    assigning better colors

--- web/static/js/highcharts.utils.js
+++ web/static/js/highcharts.utils.js
@@ -300,7 +300,7 @@ function loadReleaseDevelPieChart(container, title, data){
                 }
             }
         },
-        colors: ['#878787', '#F6AE2D','#4F1271', '#B8E2C8', '#8e1919', '#540000', '#d9534f', '#808080'],
+        colors: ['#878787', 'c12e2a', '#F6AE2D','#4F1271', '#B8E2C8', '#8e1919', '#540000', '#d9534f', '#808080'],
 
         series: [data]
     };
