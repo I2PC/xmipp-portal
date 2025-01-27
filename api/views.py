4bb4b3bfe2d51790777bddef3fad0a5b223e58fb
albertmena <12760268+albertmena@users.noreply.github.com>
2025-01-27 15:09:11 +0100

    detailed loggers

--- api/views.py
+++ api/views.py
@@ -212,18 +212,13 @@ class DetailedReleasePieChartView(APIView):
     """
 
     try:
-        successfull_attempts = Attempt.objects.filter(
-		    xmipp__id=release_id,
-		    returnCode=0,
-	).count()
-        logger.info('successfull_attempts')
-        logger.info(successfull_attempts)
-        # latest_attempt_dates = Attempt.objects.filter(
-        # 	xmipp__id=release_id,
-        # ).values('user').annotate(latest_date=Max('date'))
-        #
-        # logger.info("latest_attempt_dates: ")
-        # logger.info(latest_attempt_dates)
+
+
+        latest_attempt_dates = Attempt.objects.filter(
+        	xmipp__id=release_id,
+        ).values('user').annotate(latest_date=Max('date'))
+        logger.info("latest_attempt_dates: ")
+        logger.info(latest_attempt_dates)
         # latest_attempts = Attempt.objects.filter(
         #     Q(date__in=[item['latest_date'] for item in latest_attempt_dates]),
         #     returnCode=0,
