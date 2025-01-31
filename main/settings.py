77d5c36c95eb2a5e61c3d7aafe796ede701f5e2d
albertmena <12760268+albertmena@users.noreply.github.com>
2025-01-31 11:27:44 +0100

    Iren makina

--- main/settings.py
+++ main/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 
 import os
 from dotenv import load_dotenv
+import pymysql
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
