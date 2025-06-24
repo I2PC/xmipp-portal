5746e308c357fafdba06530a3b29de1e235fd7e8
albertmena <12760268+albertmena@users.noreply.github.com>
2025-06-24 12:52:06 +0200

    update colors

--- web/static/js/highcharts.utils.js
+++ web/static/js/highcharts.utils.js
@@ -317,7 +317,7 @@ function loadReleaseDevelPieChart(container, title, data){
                 }
             }
         },
-        colors: ['#c12e2a', '#878787', '#F6AE2D','#4F1271', '#B8E2C8', '#8e1919', '#540000', '#d9534f', '#808080'], //move 1 position the gray #878787 if new release appear
+        colors: ['#c12e2a', '#F6AE2D', '#878787','#4F1271', '#B8E2C8', '#8e1919', '#540000', '#d9534f', '#808080'], //move 1 position right the gray #878787 if new release appear
 
         series: [data]
     };
